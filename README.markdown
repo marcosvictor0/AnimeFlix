20f2eadbbdc40f2cd3f04e740df2889bb828c5f3
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-12 11:50:09 -0300

    att do codepen para o github

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# AnimeFlix2 - Funções
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/marcosvictor0/pen/rNjLONJ](https://codepen.io/marcosvictor0/pen/rNjLONJ).
+
+
---
+++ src/script.js
@@ -0,0 +1,18 @@
+function adicionarAnime() {
+  var campoAnimeFavorito = document.querySelector("#anime")
+  var animeFavorito = campoAnimeFavorito.value
+  //console.log(animeFavorito)
+  if (animeFavorito.endsWith(".jpg")) {
+    listarAnimeNaTela(animeFavorito)
+  } else {
+    alert("Imagem Inválida!")
+  }
+  campoAnimeFavorito.value = ""
+}
+
+function listarAnimeNaTela(animeFavorito) {
+  var listaAnimes = document.querySelector("#listaAnimes")
+  var elementoAnime = "<img src= " + animeFavorito + ">"
+  listaAnimes.innerHTML = listaAnimes.innerHTML + elementoAnime
+}
+
